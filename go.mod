1b921f625e2ad47393c8362ca7d3c5b82b56face
Rosun GV <gvrosun@gmail.com>
2022-03-05 18:54:19 +0530

    Stable Version
    [+] Full Application

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module Helper
 go 1.17
 
 require (
+	github.com/akavel/rsrc v0.10.2 // indirect
 	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gen2brain/shm v0.0.0-20200228170931-49f9650110c5 // indirect
@@ -10,10 +11,13 @@ require (
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jezek/xgb v0.0.0-20210312150743-0e0f116e1240 // indirect
+	github.com/josephspurrier/goversioninfo v1.4.0 // indirect
 	github.com/kbinani/screenshot v0.0.0-20210720154843-7d3a670d8329 // indirect
 	github.com/lxn/win v0.0.0-20210218163916-a377121e959e // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
@@ -25,5 +29,6 @@ require (
 	golang.org/x/sys v0.0.0-20211210111614-af8b64212486 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
+	gopkg.in/toast.v1 v1.0.0-20180812000517-0a84660828b2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+//go:generate goversioninfo -icon=exam.ico -manifest=goversioninfo.exe.manifest
 package main
 
 import (
@@ -7,12 +8,16 @@ import (
 	"github.com/atotto/clipboard"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/kbinani/screenshot"
+	"github.com/nfnt/resize"
 	"github.com/tkanos/gonfig"
+	"gopkg.in/toast.v1"
 	"image/jpeg"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"os/user"
+	"strings"
 )
 
 type Configuration struct {
@@ -28,12 +33,20 @@ var numericKeyboard = tgbotapi.NewReplyKeyboard(
 )
 
 func main() {
+	if !connected() {
+		toastNotification("No Internet Connection", "Please Check your connectivity and try again!")
+		os.Exit(1)
+	}
 	config := initBot()
-	fmt.Println(config)
+	if config.Token == "YOUR_TOKEN_HERE" {
+		toastNotification("Attention!!!", "Please provide your Telegram Bot API Token in config.json file")
+	}
 	bot, err := tgbotapi.NewBotAPI(config.Token)
 	if err != nil {
-		log.Panic(err)
+		toastNotification("Error!!!", "Please verify your Token and try again.")
+		os.Exit(1)
 	}
+	toastNotification("Bot Initialed...", "Now you are good to go, Enter /start in telegram bot.")
 
 	bot.Debug = false
 
@@ -51,7 +64,7 @@ func main() {
 			continue
 		}
 		chatId := update.Message.Chat.ID
-		if (config.UserId != 0) && !(chatId == config.UserId) {
+		if (config.UserId != 0) && !(update.Message.From.ID == config.UserId) {
 			continue
 		}
 
@@ -59,7 +72,13 @@ func main() {
 
 		switch update.Message.Command() {
 		case "help":
-			msg.Text = "I understand /send and /clip."
+			msg.Text = "I understand these commands:\n" +
+				"/start - Start Bot and Enable Shortcut\n" +
+				"/send - Send captured Screenshot\n" +
+				"/clip - Send Clipboard data\n" +
+				"/close - Close the Shortcut buttons\n" +
+				"/exit - Stop Bot Application completely\n" +
+				"/help - Know more about each commands"
 		case "send":
 			sendPhoto(chatId, bot)
 			continue
@@ -68,15 +87,23 @@ func main() {
 			msg.Text += "\n\nFrom: " + getUsername()
 			msg.ParseMode = "markdown"
 		case "start":
-			msg.Text = "Bot Started 😁"
+			msg.Text = "Bot Working...\n" +
+				"List of Commands:\n" +
+				"/start - Start Bot and Enable Shortcut\n" +
+				"/send - Send captured Screenshot\n" +
+				"/clip - Send Clipboard data\n" +
+				"/close - Close the Shortcut buttons\n" +
+				"/exit - Stop Bot Application completely\n" +
+				"/help - Know more about each commands"
 			msg.ReplyMarkup = numericKeyboard
 		case "close":
 			msg.Text = "Shortcut Closed"
 			msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
 		case "exit":
+			toastNotification("Bot Terminated...", "Bot exited and cleared from memory...")
 			os.Exit(0)
 		default:
-			msg.Text = "I don't know that command" + getUsername()
+			msg.Text = "I don't know that command"
 		}
 		if _, err := bot.Send(msg); err != nil {
 			log.Panic(err)
@@ -93,6 +120,7 @@ func initBot() (config Configuration) {
 		}
 		file, _ := json.MarshalIndent(newConfig, "", " ")
 		_ = ioutil.WriteFile("config.json", file, 0644)
+		toastNotification("Attention Needed!!!", "Config.json file is generated, Please review the file to fill the details required")
 		os.Exit(1)
 	}
 	return
@@ -100,15 +128,16 @@ func initBot() (config Configuration) {
 
 func sendPhoto(chatId int64, bot *tgbotapi.BotAPI) {
 	img, err := screenshot.CaptureDisplay(0)
+	resizeImg := resize.Resize(1280, 720, img, resize.Lanczos3)
 	if err != nil {
 		panic(err)
 	}
 	buf := new(bytes.Buffer)
-	err = jpeg.Encode(buf, img, nil)
+	err = jpeg.Encode(buf, resizeImg, nil)
 	photoBytes := buf.Bytes()
 
 	photoFileBytes := tgbotapi.FileBytes{
-		Name:  "picture",
+		Name:  "ScreenShot",
 		Bytes: photoBytes,
 	}
 	msg := tgbotapi.NewPhoto(chatId, photoFileBytes)
@@ -124,6 +153,7 @@ func clipString() (string, tgbotapi.InlineKeyboardMarkup) {
 	if err != nil {
 		text = "Something Went Wrong!!!"
 	}
+	text = strings.Replace(text, "\"", "'", -1)
 	clipButton := tgbotapi.NewInlineKeyboardMarkup(
 		tgbotapi.NewInlineKeyboardRow(
 			tgbotapi.NewInlineKeyboardButtonURL("Search Google", "https://www.google.com/search?q="+text),
@@ -138,4 +168,29 @@ func getUsername() string {
 		userData.Name = "Anonymous"
 	}
 	return userData.Name
+}
+
+func toastNotification(title string, message string) {
+	notification := toast.Notification{
+		AppID:   "Go Helper",
+		Title:   title,
+		Message: message,
+		Icon:    "R:\\Development\\Go\\Helper\\exam.png",
+		Audio:   toast.Reminder,
+		Actions: []toast.Action{
+			{"protocol", "Exit", ""},
+		},
+	}
+	err := notification.Push()
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func connected() (ok bool) {
+	_, err := http.Get("http://clients3.google.com/generate_204")
+	if err != nil {
+		return false
+	}
+	return true
 }
